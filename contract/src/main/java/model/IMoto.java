a53a51f41c32bca7910865540dd20483bb73ef38
vincentchatel <33131154+vincentchatel@users.noreply.github.com>
2018-07-11 14:41:29 +0200

    Imoto

--- contract/src/main/java/model/IMoto.java
+++ contract/src/main/java/model/IMoto.java
@@ -1,5 +1,21 @@
 package model;
 
 public interface IMoto {
+	
+	void reset();
+	
+	int getX();
+	
+	void setX(int x);
+	
+	int getY();
+	
+	void setY(int y);
+	
+	Direction getDirection();
+	
+	void setDirection(Direction directon);
+	
+	String getName();
 
 }
