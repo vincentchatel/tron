7ed121a9f469ad95b69404e024991411918266c0
vincentchatel <33131154+vincentchatel@users.noreply.github.com>
2018-07-11 17:30:17 +0200

    model facade

--- model/src/main/java/model/ModelFacade.java
+++ model/src/main/java/model/ModelFacade.java
@@ -25,15 +25,15 @@ public final class ModelFacade implements IModel {
      * @see model.IModel#getExampleById(int)
      */
     @Override
-    public Example getExampleById(final int id) throws SQLException {
-        return ExampleDAO.getExampleById(id);
+    public void sendDataGame(String name, int time) throws SQLException {
+         ExampleDAO.sendDataGame(name, time);
     }
 
     /*
      * (non-Javadoc)
      * @see model.IModel#getExampleByName(java.lang.String)
      */
-    @Override
+    /*@Override
     public Example getExampleByName(final String name) throws SQLException {
         return ExampleDAO.getExampleByName(name);
     }
@@ -41,10 +41,10 @@ public final class ModelFacade implements IModel {
     /*
      * (non-Javadoc)
      * @see model.IModel#getAllExamples()
-     */
-    @Override
-    public List<Example> getAllExamples() throws SQLException {
-        return ExampleDAO.getAllExamples();
+    /* */
+   
+    public IMoto createMoto(int joueur, String nom) {
+    	return new Moto(joueur, nom);
     }
 
 }
