d47102245aee8202a6e2396d1f850d55058722a6
vincentchatel <33131154+vincentchatel@users.noreply.github.com>
2018-07-11 15:27:50 +0200

    controller

--- controller/src/main/java/controller/ControllerFacade.java
+++ controller/src/main/java/controller/ControllerFacade.java
@@ -20,6 +20,12 @@ public class ControllerFacade implements IController {
 
     /** The model. */
     private final IModel model;
+    
+    private String vainqueur;
+    
+    private int tempsFinal;
+    
+    private Game game;
 
     /**
      * Instantiates a new controller facade.
@@ -42,19 +48,104 @@ public class ControllerFacade implements IController {
      *             the SQL exception
      */
     public void start() throws SQLException {
-        this.getView().displayMessage(this.getModel().getExampleById(1).toString());
+        //this.getView().displayMessage(this.getModel().getExampleById(1).toString());
 
-        this.getView().displayMessage(this.getModel().getExampleByName("Example 2").toString());
+       // this.getView().displayMessage(this.getModel().getExampleByName("Example 2").toString());
 
-        final List<Example> examples = this.getModel().getAllExamples();
+       /* final List<Example> examples = this.getModel().getAllExamples();
         final StringBuilder message = new StringBuilder();
         for (final Example example : examples) {
             message.append(example);
             message.append('\n');
         }
-        this.getView().displayMessage(message.toString());
+        this.getView().displayMessage(message.toString());*/
+    
+    	IGame g = new Game(this.getModel().createMoto(1, "Player1"), this.getModel().createMoto(2, "Player2"));
+    	this.getView().setInputListener(g);
+    	System.out.println("test reset");
+    	g.reset();
+    	
+    	
+    	System.out.println("test avant create");
+    	this.getView().createWindow(g);
+    	System.out.println("test après create");
+    	
+    	long currentTime;
+    	double dt;
+    	double fps = 60.0;
+    	long lastTime = -1;
+    	
+    	while(!g.isGameOver()) {
+    		currentTime = System.currentTimeMillis();
+    				dt = (currentTime - lastTime)/1000.0;
+    				
+    				if (dt*fps >= 1.0 ) {
+    					lastTime = currentTime;
+    					System.out.println("Update While");
+    					g.update();
+    				}
+    	}
+    	
+    	String nom;
+    	if (g.getWinner() == null) {
+    		nom = "Pas de vainqueur";
+    	}
+    	else {
+    		nom = g.getWinner().getName();
+    	}
+    	this.getModel().sendDataGame(nom, g.getTempsFinale());
+    	
     }
-
+    	
+    	
+    	public IView getView() {
+    		return this.view;	
+    	}
+    	
+    	
+    	public IModel getModel() {
+    		return this.model;
+    	}
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /**
      * Gets the view.
      *
