73b42b56e2d14996e5ceb608023225c70b65374e
vincentchatel <33131154+vincentchatel@users.noreply.github.com>
2018-07-12 17:11:16 +0200

    test java

---
+++ model/src/test/java/model/ExampleTest.java
@@ -0,0 +1,27 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+/**
+ * 
+ * @author Vincent
+ *
+ */
+
+public class ExampleTest {
+	
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
