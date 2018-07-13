b0ae7fd7ed065bd3df8e095e3f638e190ddc54dd
vincentchatel <33131154+vincentchatel@users.noreply.github.com>
2018-07-13 02:48:55 +0200

    test unitaire

--- model/src/test/java/model/ExampleTest.java
+++ model/src/test/java/model/ExampleTest.java
@@ -1,27 +1,79 @@
 package model;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
-import org.junit.Test;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * 
- * @author Vincent
+ * @author Ludovic Pierson
  *
  */
-
 public class ExampleTest {
-	
-	
-	
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    private Example example;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * 
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        this.example = new Example(1, "player1");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testGetId() {
+        final int expected = 0;
+        assertEquals(expected, this.example.getId());
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testGetName() {
+        final String expected = "player1";
+        assertEquals(expected, this.example.getName());
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testSetName() {
+        String expected = "player1";
+        assertEquals(expected, this.example.getName());
+        expected = "player2";
+        this.example.setName(expected);
+        assertEquals(expected, this.example.getName());
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testToString() {
+        final String expected = "1 s : vainqueur player1";
+        assertEquals(expected, this.example.toString());
+    }
 
 }
